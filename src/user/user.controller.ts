0ae5c7360d0cd7feafadfc908539177aaeea9623
unknown <borges10002@gmail.com>
2025-01-21 20:59:34 -0400

    fist project

---
+++ src/user/user.controller.ts
@@ -0,0 +1,9 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+  @Post()
+  async create(@Body() body) {
+    return { body };
+  }
+}
