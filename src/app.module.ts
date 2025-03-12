c57c4f47ace126697370b9ca262f6834a1863fd0
Borges10002 <borges10002@gmail.com>
2025-03-11 20:23:33 -0400

    nodemail

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,31 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     forwardRef(() => UserModule),
     forwardRef(() => AuthModule),
+
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport:
+          'smtps://cecelia.crona59@ethereal.email:nURXTVyjvgJqfkt8em@smtp.ethereal.email',
+        defaults: {
+          from: '"nest-modules" <modules@nestjs.com>',
+        },
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new PugAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
